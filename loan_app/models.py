cab5006a9a62f88c1457d49cd771312aeb2be4a6
Diego Marcelino <diego.arm.mar@gmail.com>
2019-04-19 16:28:12 -0300

    Added Loan and Payment models in order to get dfatabase.

--- loan_app/models.py
+++ loan_app/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+
+
+class Loan(models.Model):
+  amount = models.DecimalField(max_digits=8, decimal_places=2, null=False)
+  balance = models.DecimalField(max_digits=8, decimal_places=2, null=False)
+  term = models.IntegerField(null=False)
+  rate = models.DecimalField(max_digits=4, decimal_places=3, null=False)
+  date = models.DateTimeField(auto_now=False, null=False)
+  installment = models.DecimalField(max_digits=8, decimal_places=2, null=False)
+  paid = models.BooleanField(default=False)
+
+
+class Payment(models.Model):
+  loan = models.ForeignKey(Loan, on_delete=models.PROTECT)
+  made = models.BooleanField(null=False)
+  date = models.DateTimeField(auto_now=False, null=False)
+  amount = models.DecimalField(max_digits=8, decimal_places=2, null=False)
+  
