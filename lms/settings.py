fd682387727b4058f52f4d76e8eb0623d6101597
Diego Marcelino <diego.arm.mar@gmail.com>
2019-04-19 15:18:41 -0300

    Server first run. Added unecessary files to gitignore.

--- lms/settings.py
+++ lms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*$i+*mjv!c8y=%od*6$5l!15#puy-zy7714iwj@&+d0bs84cm$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
