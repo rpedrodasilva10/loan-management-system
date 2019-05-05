3f0de7a44e4ab6f2f15706a01f7cd559487ffeef
edulira <duslira@gmail.com>
2019-05-05 12:14:38 -0300

    create serializer for Loan model

--- loan_app/serializers.py
+++ loan_app/serializers.py
@@ -1,14 +1,35 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+from loan_app.models import Loan
 
-class LoanSerializer(serializers.HyperlinkedModelSerializer):
+class LoanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Loan
-        fields = ('')
+        fields = [
+            'id',
+            'amount',
+            'term',
+            'rate',
+            'date',
+            'installment',
+        ]
+        read_only_fields = ['id', 'installment']
 
 
-class PaymentSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Payment
-        fields = ('')
+    def create(self, validated_data):
+        print(type(validated_data.get('rate')))
+        _rate = float(validated_data.get('rate'))
+        _term = int(validated_data.get('term'))
+        _amount = float(validated_data.get('amount'))
+        r = _rate / 12.0
+        _installment = ((r + r / ((1 + r) ** _term - 1)) * _amount)
+
+        loan = Loan.objects.create(
+            amount = _amount,
+            term = _term,
+            rate = _rate,
+            date = validated_data.get('date'),
+            installment = _installment
+        )
+        
+        return loan
