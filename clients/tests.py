e6e46757480980fcf445bd77e841a0b4ecd15b48
CAROLINA SCHWAAB MARCAL <carolina.marcal@sistemafiesc.org.br>
2019-05-14 21:47:19 -0300

    test client

--- clients/tests.py
+++ clients/tests.py
@@ -1,20 +1,48 @@
 from django.test import TestCase
-
-# Create your tests here.
+from pycpfcnpj import gen
 from clients.models import Client
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from rest_framework.test import RequestsClient
+from rest_framework.test import force_authenticate
+from django.contrib.auth.models import User
+import json
+
 
 class ClientTest(TestCase):
     """ Test module for Client model """
 
     def setUp(self):
+        self.password = '123'
+        self.user= User.objects.create_user(
+            'carolina', 'carol@gmail.com', self.password)
+
         Client.objects.create(
             name='Felicity', surname='Jones', email='felicity@gmail.com', telephone='11984345678', cpf='34598712387')
-        
 
-    def test_puppy_breed(self):
+    def test_create_client(self):
         client_jones = Client.objects.get(name='Felicity')
-        print(client_jones)
 
         self.assertEqual(
-            client_jones.get_client(), "Felicity Jones e-mail felicity@gmail.com telephone 11984345678 cpf 34598712387")
-       
+            client_jones.__str__(), "1 - Felicity Jones")
+
+    def test_post_valid_client(self):
+        client = APIClient()
+        client.login(username=self.user.username, password=self.password)
+        response = client.post(
+            reverse('client-create'),
+             json.dumps({'name':'Luis','surname':'Martins','email':'lu@gmail.com', 'telephone':'51981790346', 'cpf': gen.cpf()}),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_invalid_client(self):
+        client = APIClient()
+        client.login(username=self.user.username, password=self.password)
+        response = client.post(
+            reverse('client-create'),
+            json.dumps({'name': '','surname': 'Gonzalves','email': 'lu@gmail.com', 'telephone': '1199345678', 'cpf': '26985367054'}),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
