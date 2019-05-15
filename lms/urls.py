f75cd82475c47ce486321f7cdf67774607243750
Unknown <bruno.fiorucci@outlook.com>
2019-05-15 11:22:54 -0300

    API docs

--- lms/urls.py
+++ lms/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('loan_app.urls')),
     path('clients/', include('clients.urls')),
+    path('docs/', include_docs_urls(title='Loan Management System', public=True, permission_classes=[])),
 ]
