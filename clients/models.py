a5c420f58c44d73713096d43d07b56f69a42ff83
Unknown <bruno.fiorucci@outlook.com>
2019-05-16 08:26:22 -0300

    clients field desc

--- clients/models.py
+++ clients/models.py
@@ -15,12 +15,12 @@ class Client(Base):
     """
     Client class abstracts a client in the system
     """
-    client_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    email = models.EmailField()
-    telephone = models.CharField(max_length=11)
-    cpf = models.CharField("CPF", max_length=11, unique=True)
+    client_id = models.AutoField(primary_key=True, help_text="unique id of a client.")
+    name = models.CharField(max_length=100, help_text="the client name.")
+    surname = models.CharField(max_length=100, help_text="the client surname.")
+    email = models.EmailField(help_text="the client email.")
+    telephone = models.CharField(max_length=11, help_text="the client telephone.")
+    cpf = models.CharField("CPF", max_length=11, unique=True, help_text="the client identification.")
 
     def __str__(self):
         return f'{self.client_id} - {self.name} {self.surname}'
