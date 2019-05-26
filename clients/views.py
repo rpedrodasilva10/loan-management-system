293bff31ebd0ead844289c09daf288c3929e127f
Renan <rpedrodasilva10@gmail.com>
2019-05-25 21:21:22 -0300

    GET clients (show all clientes) implementedand migrations updated

--- clients/views.py
+++ clients/views.py
@@ -2,15 +2,19 @@
 Views for clients application.
 """
 
-from rest_framework import generics, status
+from rest_framework import generics, status, views
 from rest_framework.response import Response
 from .serializers import ClientSerializer
+from .models import Client
 
-class ClientAPIView(generics.CreateAPIView):
+
+class ClientAPIView(views.APIView):
     """
     Create a new client.
     """
     serializer_class = ClientSerializer
+
+    queryset = Client.objects.all()
 
     def post(self, request, *args, **kwargs):
         serializer = ClientSerializer(data=request.data)
@@ -21,3 +25,24 @@ class ClientAPIView(generics.CreateAPIView):
             }
             return Response(content, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, *args, **kwargs):
+        content = []
+        client_id = self.kwargs.get("client_id")
+        if None == client_id:
+            clients = Client.objects.all()
+        else:
+            clients = Client.objects.get(client_id=client_id)
+        for client in clients:
+            content.append(
+                {
+                    "client_id": client.client_id,
+                    "name": client.name,
+                    "surname": client.surname,
+                    "email": client.email,
+                    "telephone": client.telephone,
+                    "cpf": client.cpf
+                }
+            )
+
+        return Response(content, status=status.HTTP_200_OK)
